e149948ea0f579d84b4abc83ff504947c0e966b3
Sandra Tequiliquihua Ajactle <sandratequi.ajactle@gmail.com>
2026-04-27 13:07:49 -0600

    Cambiando el modelo a gemini-pro

--- SKINCARE/netlify/functions/chat.js
+++ SKINCARE/netlify/functions/chat.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context) {
 
         // Tomamos tu clave secreta de las variables de entorno de Netlify
         const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-        const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+        const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`;
 
         // Hacemos la petición a Google Gemini
         const response = await fetch(API_URL, {
